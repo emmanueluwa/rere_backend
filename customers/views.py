7532cf7670340850e58a598d69a7a64d28feb86b
emmanueluwa <emmanuel@fulodev.com>
2023-12-31 19:55:40 +0000

    creating detail by id API

--- customers/views.py
+++ customers/views.py
@@ -3,8 +3,14 @@ from django.http import JsonResponse
 from customers.serializers import CustomerSerializer
 
 def customers(request):
-  #invoking serializer and returning client
   data = Customer.objects.all()
   serializer = CustomerSerializer(data, many=True)
   #get serialized version, passing json compatible data as response
   return JsonResponse({"customers": serializer.data})
+
+
+def customer(request, id):
+  data = Customer.objects.get(pk=id)
+  serializer = CustomerSerializer(data)
+  #get serialized version, passing json compatible data as response
+  return JsonResponse({"customer": serializer.data})

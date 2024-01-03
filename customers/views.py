c97e32557cb4cea428d7236d54b14cbeab9be9f4
emmanueluwa <emmanuel@fulodev.com>
2024-01-03 05:52:45 +0000

    adding refresh and acces for user registration

--- customers/views.py
+++ customers/views.py
@@ -5,6 +5,7 @@ from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.permissions import IsAuthenticated
+from rest_framework_simplejwt.tokens import RefreshToken
 
 
 #DEFINFING: an api that can take get, post requests using API decorator
@@ -63,5 +64,14 @@ def customer(request, id):
 def register(request):
   serializer = UserSerializer(data=request.data)
   if serializer.is_valid():
-    serializer.save()
-    return Response(status=status.HTTP_201_CREATED)
+    # creating refresh and access tokens for when user registers
+    user = serializer.save()
+    refresh = RefreshToken.for_user(user)
+    tokens = {
+      "refresh": str(refresh),
+      "access": str(refresh.access_token)
+    }
+    return Response(tokens, status=status.HTTP_201_CREATED)
+  
+  #if serializer not valid
+  return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)

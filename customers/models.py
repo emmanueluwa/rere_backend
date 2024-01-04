af82a19e2f2c6607787857846c833718fc225356
emmanueluwa <emmanuel@fulodev.com>
2024-01-04 16:09:51 +0000

    adding orders for each customer using graphql nested data

--- customers/models.py
+++ customers/models.py
@@ -3,3 +3,17 @@ from django.db import models
 class Customer(models.Model):
   name = models.CharField(max_length=100)
   industry = models.CharField(max_length=100)
+
+  #admin display name instead of "object(2)"
+  def __str__(self):
+    return self.name
+
+class Order(models.Model):
+  customer = models.ForeignKey(Customer, related_name="orders", on_delete=models.CASCADE)
+  description = models.CharField(max_length=500)
+  #to avoid using float: use pence and convert to pound(/100), nodecimal type in sqllite3
+  totalInPence = models.IntegerField()
+
+  #admin display name instead of "object(2)"
+  def __str__(self):
+    return self.description
